e622ffc7234eed3d31a83a54d25e819823f758e7
cmbsolver <cmbsolver@gmail.com>
2024-05-19 22:20:17 -0500

    Color report adjustments.

--- LiberPrimusUi/Views/ColorReportView.axaml.cs
+++ LiberPrimusUi/Views/ColorReportView.axaml.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
@@ -29,6 +30,31 @@ public partial class ColorReportView : UserControl
         if (files.Count >= 1)
         {
             viewModel.FileName = files[0].TryGetLocalPath();
+        }
+    }
+
+    private async void Save_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var viewModel = (ColorReportViewModel)DataContext;
+        
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog.
+        var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+        {
+            Title = "Save Text File"
+        });
+
+        if (file is not null)
+        {
+            // Open writing stream from the file.
+            await using var stream = await file.OpenWriteAsync();
+            using var streamWriter = new StreamWriter(stream);
+            // Write some content to the file.
+            await streamWriter.WriteAsync(viewModel.Result);
+            await streamWriter.FlushAsync();
+            streamWriter.Close();
         }
     }
 }
