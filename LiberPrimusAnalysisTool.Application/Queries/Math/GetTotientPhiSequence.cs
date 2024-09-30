fd8781b8fca1f906a8b53d69deb1519b80fc4e3c
cmbsolver <cmbsolver@gmail.com>
2024-09-30 12:53:01 -0500

    Adding totient Phi list.

---
+++ LiberPrimusAnalysisTool.Application/Queries/Math/GetTotientPhiSequence.cs
@@ -0,0 +1,125 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LiberPrimusAnalysisTool.Application.Interfaces;
+using LiberPrimusAnalysisTool.Entity;
+using MediatR;
+
+namespace LiberPrimusAnalysisTool.Application.Queries.Math
+{
+    /// <summary>
+    /// Get Totient Sequence
+    /// </summary>
+    public class GetTotientPhiSequence : ISequence
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public static string Name => "Totient Phi";
+        
+        // <summary>
+        /// Get whether the sequence is positional.
+        /// </summary>
+        public static bool IsPositional { get; set; }
+
+        /// <summary>
+        /// Builds the command.
+        /// </summary>
+        /// <param name="number">The number.</param>
+        /// <returns></returns>
+        public static object BuildCommand(ulong number, bool isPostional)
+        {
+            var query = new Query(number, isPostional);
+            IsPositional = isPostional;
+
+            return query;
+        }
+
+        /// <summary>
+        /// Query
+        /// </summary>
+        public class Query : IRequest<NumericSequence>
+        {
+            public Query(ulong maxNumber, bool isPositional)
+            {
+                MaxNumber = maxNumber;
+                IsPositional = isPositional;
+            }
+            
+            /// <summary>
+            /// Gets or sets the maximum n number.
+            /// </summary>
+            /// <value>
+            /// The maximum n number.
+            /// </value>
+            public ulong MaxNumber { get; set; }
+            
+            /// <summary>
+            /// Gets whether it is positional.
+            /// </summary>
+            public bool IsPositional { get; set; }
+        }
+
+        /// <summary>
+        /// Handler
+        /// </summary>
+        public class Handler : IRequestHandler<Query, NumericSequence>
+        {
+            private readonly IMediator _mediator;
+            
+            public Handler(IMediator mediator)
+            {
+                _mediator = mediator;
+            }
+            
+            /// <summary>
+            /// Handles a request
+            /// </summary>
+            /// <param name="request">The request</param>
+            /// <param name="cancellationToken">Cancellation token</param>
+            /// <returns>
+            /// Response from the request
+            /// </returns>
+            public async Task<NumericSequence> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var totient = new NumericSequence(Name);
+
+                for (ulong i = 0; i <= request.MaxNumber; i++)
+                {
+                    try
+                    {
+                        var retval = await _mediator.Send(new GetTotientSequence.Query(i, request.IsPositional));
+                        totient.Sequence.Add(Convert.ToUInt64(retval.Result));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    
+                }
+
+                return totient;
+            }
+
+            /// <summary>
+            /// GCDs the specified a.
+            /// </summary>
+            /// <param name="a">a.</param>
+            /// <param name="b">The b.</param>
+            /// <returns></returns>
+            private ulong GCD(ulong a, ulong b)
+            {
+                while (b != 0)
+                {
+                    var t = b;
+                    b = a % b;
+                    a = t;
+                }
+
+                return a;
+            }
+        }
+    }
+}
