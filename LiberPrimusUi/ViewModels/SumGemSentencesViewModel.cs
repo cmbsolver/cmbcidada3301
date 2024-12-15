e5034ec94fbf44e667765842f286f4d8f20dacf1
cmbsolver <cmbsolver@gmail.com>
2024-12-15 12:39:59 -0600

    Clear button fix.

--- LiberPrimusUi/ViewModels/SumGemSentencesViewModel.cs
+++ LiberPrimusUi/ViewModels/SumGemSentencesViewModel.cs
@@ -45,5 +45,6 @@ public partial class SumGemSentencesViewModel: ViewModelBase
     public async void ClearText()
     {
         Response = string.Empty;
+        TextToTranspose = string.Empty;
     }
 }
