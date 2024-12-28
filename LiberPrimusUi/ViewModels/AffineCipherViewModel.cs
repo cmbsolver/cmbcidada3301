e26d8e562e9524f484282cdd1e7263ed162f9fd5
cmbsolver <cmbsolver@gmail.com>
2024-12-28 17:17:38 -0600

    Added better brute force.

--- LiberPrimusUi/ViewModels/AffineCipherViewModel.cs
+++ LiberPrimusUi/ViewModels/AffineCipherViewModel.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LiberPrimusAnalysisTool.Application.Commands.Decoders;
 using LiberPrimusAnalysisTool.Application.Commands.Encoders;
+using LiberPrimusAnalysisTool.Application.Commands.TextUtilies;
+using LiberPrimusAnalysisTool.Database;
+using LiberPrimusAnalysisTool.Entity.Text;
 using MediatR;
 
 namespace LiberPrimusUi.ViewModels;
@@ -45,10 +49,21 @@ public partial class AffineCipherViewModel: ViewModelBase
         if (string.IsNullOrEmpty(Shift) || string.IsNullOrEmpty(Multiplier) ||
             string.IsNullOrWhiteSpace(Shift) || string.IsNullOrWhiteSpace(Multiplier))
         {
-            DecodedString = "";
-            for (int i = 0; i < Alphabet.Length; i++)
+            DecodedString = $"Multiplier and Shift were not set! Using Brute Force to decode...\n";
+            string[] alphaArray = Alphabet.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            List<string> dict = new();
+            List<TextScore> textScores = new();
+            using (var context = new LiberContext())
             {
-                for (int j = 0; j < Alphabet.Length; j++)
+                dict.AddRange(context.DictionaryWords.Select(x => x.DictionaryWordText).ToList());
+                dict.AddRange(context.DictionaryWords.Select(x => x.RuneglishWordText).ToList());
+                dict.AddRange(context.DictionaryWords.Select(x => x.RuneWordText).ToList());
+            }
+            
+            DecodedString = "";
+            for (int i = 0; i < alphaArray.Length; i++)
+            {
+                for (int j = 0; j < alphaArray.Length; j++)
                 {
                     try
                     {
@@ -57,8 +72,11 @@ public partial class AffineCipherViewModel: ViewModelBase
                             i, 
                             j, 
                             Alphabet);
-                        DecodedString += await _mediator.Send(command);
-                        DecodedString += Environment.NewLine;
+                        
+                        string decoded = await _mediator.Send(command);
+                        var score = await _mediator.Send(new ScoreText.Command(decoded, dict));
+                        var textScore = new TextScore(StringToDecode, decoded, score.Item1, "", score.Item2);
+                        textScores.Add(textScore);
                     }
                     catch (Exception e)
                     {
@@ -67,6 +85,11 @@ public partial class AffineCipherViewModel: ViewModelBase
                 }
             }
             
+            textScores = textScores.OrderByDescending(x => x.Score).ToList();
+            foreach (var textScore in textScores)
+            {
+                DecodedString += $"{textScore.Score} - {textScore.Text}\n";
+            }
         }
         else
         {
