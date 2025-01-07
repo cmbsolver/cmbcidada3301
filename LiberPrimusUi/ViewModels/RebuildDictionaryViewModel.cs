d43848a8cc01ca4b81bd4526ed6530aa24e5ba1b
cmbsolver <cmbsolver@gmail.com>
2025-01-07 17:05:17 -0600

    Added ability to export and load custom dictionaries.

--- LiberPrimusUi/ViewModels/RebuildDictionaryViewModel.cs
+++ LiberPrimusUi/ViewModels/RebuildDictionaryViewModel.cs
@@ -4,12 +4,14 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LiberPrimusAnalysisTool.Application.Commands.Decoders;
 using LiberPrimusAnalysisTool.Application.Commands.Encoders;
 using LiberPrimusAnalysisTool.Application.Commands.TextUtilies;
 using LiberPrimusAnalysisTool.Database;
+using LiberPrimusAnalysisTool.Entity.Text;
 using MediatR;
 
 namespace LiberPrimusUi.ViewModels;
@@ -32,5 +34,73 @@ public partial class RebuildDictionaryViewModel: ViewModelBase
         IndexWordDirectory.Command command = new();
         await _mediator.Publish(command);
         DecodedString = "Dictionary rebuilt!";
+    }
+    
+    public async Task ExportDict(string outputFile)
+    {
+        DecodedString = "Exporting dictionary...";
+        
+        using (var context = new LiberContext())
+        {
+            var words = context.DictionaryWords.ToList();
+            XLWorkbook workbook = new();
+            var worksheet = workbook.Worksheets.Add("Dictionary");
+            
+            worksheet.Cell(1, 1).Value = "Word";
+            worksheet.Cell(1, 2).Value = "Runeglish";
+            worksheet.Cell(1, 3).Value = "Rune";
+            
+            for (int i = 0; i < words.Count; i++)
+            {
+                worksheet.Cell(i + 2, 1).Value = words[i].DictionaryWordText;
+                worksheet.Cell(i + 2, 2).Value = words[i].RuneglishWordText;
+                worksheet.Cell(i + 2, 3).Value = words[i].RuneWordText;
+            }
+            
+            workbook.SaveAs(outputFile);
+            workbook.Dispose();
+        }
+        
+        DecodedString = $"Dictionary exported to {outputFile}";
+    }
+    
+    public async Task LoadCustomDict(string inputFile)
+    {
+        DecodedString = "Loading custom dictionary...";
+        
+        using (var context = new LiberContext())
+        {
+            foreach (var remWord in context.DictionaryWords)
+            {
+                context.DictionaryWords.Remove(remWord);
+            }
+
+            await context.SaveChangesAsync();
+            
+            var workbook = new XLWorkbook(inputFile);
+            var worksheet = workbook.Worksheet(1);
+            var rows = worksheet.RowsUsed().ToList();
+            
+            for (int i = 1; i < rows.Count; i++)
+            {
+                var word = new DictionaryWord()
+                {
+                    DictionaryWordText = worksheet.Cell(i + 1, 1).Value.ToString(),
+                    RuneglishWordText = worksheet.Cell(i + 1, 2).Value.ToString(),
+                    RuneWordText = worksheet.Cell(i + 1, 3).Value.ToString(),
+                };
+                
+                word.GemSum = Convert.ToInt64(await _mediator.Send(new CalculateGematriaSum.Command(word.RuneWordText)));
+                word.DictionaryWordLength = word.DictionaryWordText.Length;
+                word.RuneglishWordLength = word.RuneglishWordText.Length;
+                word.RuneWordLength = word.RuneWordText.Length;
+                
+                context.DictionaryWords.Add(word);
+            }
+            
+            await context.SaveChangesAsync();
+        }
+        
+        DecodedString = $"Custom dictionary loaded from {inputFile}";
     }
 }
--- LiberPrimusUi/Views/RebuildDictionaryView.axaml.cs
+++ LiberPrimusUi/Views/RebuildDictionaryView.axaml.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 using LiberPrimusUi.ViewModels;
 
 namespace LiberPrimusUi.Views;
@@ -10,5 +13,60 @@ public partial class RebuildDictionaryView : UserControl
     public RebuildDictionaryView()
     {
         InitializeComponent();
+    }
+
+    private async void ExportButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var viewModel = (RebuildDictionaryViewModel)DataContext;
+        
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog.
+        var options = new FilePickerSaveOptions();
+        options.Title = "Save Excel File";
+        options.DefaultExtension = ".xlsx";
+        options.SuggestedFileName = "DictionaryExport";
+        options.FileTypeChoices = new List<FilePickerFileType>()
+        {
+            new FilePickerFileType(".xlsx")
+            {
+                Patterns = ["*.xlsx"]
+            }
+        };
+        
+        var file = await topLevel.StorageProvider.SaveFilePickerAsync(options);
+
+        if (file is not null)
+        {
+            await viewModel.ExportDict(file.Path.AbsolutePath);
+        }
+    }
+
+    private async void LoadCustomDict_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var viewModel = (RebuildDictionaryViewModel)DataContext;
+        
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog.
+        var options = new FilePickerOpenOptions();
+        options.Title = "Open Excel File";
+        options.AllowMultiple = false;
+        options.FileTypeFilter = new List<FilePickerFileType>()
+        {
+            new FilePickerFileType(".xlsx")
+            {
+                Patterns = ["*.xlsx"]
+            }
+        };
+        
+        var file = await topLevel.StorageProvider.OpenFilePickerAsync(options);
+
+        if (file is not null)
+        {
+            await viewModel.LoadCustomDict(file[0].Path.AbsolutePath);
+        }
     }
 }
